27342b8f6308507db8e7ad7db2108cffe112b7b8
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-06-10 15:55:33 -0700

    added heroku stuff

--- static.js
+++ static.js
@@ -1,3 +1,42 @@
+// HEROKU STUFF
+// npm packages
+const express = require("express");
+const morgan = require("morgan");
+
+// globals
+const PORT = process.env.PORT || 3000; // heroku will assign a port env variable
+const app = express();
+
+// app config
+app.use(morgan("tiny"));
+
+app.get("/", (req, res, next) => {
+  return res.json("DEPLOYED!");
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  return next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  return res.json({
+    message: err.message,
+    /*
+     if we're in development mode, include stack trace (full error object)
+     otherwise, it's an empty object so the user doesn't see all of that
+    */
+    error: app.get("env") === "development" ? err : {}
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 window.onload = function(){ 
 
